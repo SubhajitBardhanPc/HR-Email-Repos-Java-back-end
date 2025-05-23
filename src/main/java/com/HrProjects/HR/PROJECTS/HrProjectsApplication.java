bb5994b6ce25092a962e7aa4bac18ca4e264fa2f
SubhajitBardhanPc <bardhansubhajit3@gmail.com>
2025-05-23 08:00:20 +0530

    initial Commit

---
+++ src/main/java/com/HrProjects/HR/PROJECTS/HrProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.HrProjects.HR.PROJECTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HrProjects/HR/PROJECTS/HrProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HrProjects.HR.PROJECTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
